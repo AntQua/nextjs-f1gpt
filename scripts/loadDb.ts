c6ce37cfb502303c005d8b929f7c373d5bf2a7a8
Antonio Quaresma <tozequa@gmail.com>
2025-01-03 10:58:11 +0000

    define websites to scrape and connect to db logic

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -16,3 +16,24 @@ const {
 
 
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY})
+
+// Define Websites to scrape
+const f1Data = [
+    'https://en.wikipedia.org/wiki/Formula_One',
+    'https://www.skysports.com/f1/news/12433/13117256/lewis-hamilton-says-move-to-ferrari-from-mercedes-doesnt-need-vindicating-amid-irritation-at-coverage',
+    'https://www.formula1.com/en/latest/all',
+    'https://www.forbes.com/sites/brettknight/2023/11/29/formula-1s-highest-paid-drivers-2023/',
+    'https://www.autosport.com/f1/news/history-of-female-f1-drivers-including-grand-prix-starters-and-test-drivers/10584871/',
+    'https://en.wikipedia.org/wiki/2023_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/2022_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/List_of_Formula_One_World_Drivers%27_Champions',
+    'https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship',
+    'https://www.formula1.com/en/results.html/2024/races.html',
+    'https://www.formula1.com/en/racing/2024.html'
+  ];
+  
+  // Connect to DB
+
+  const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN)
+
+  const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE})
